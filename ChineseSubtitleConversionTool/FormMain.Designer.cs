fb869ff20f0ceef154076fa93127a3e0c04949d6
Chishin <pro@xxgzs.org>
2019-09-07 19:25:42 +0800

    完善基础简体繁体转换功能

--- ChineseSubtitleConversionTool/FormMain.Designer.cs
+++ ChineseSubtitleConversionTool/FormMain.Designer.cs
@@ -28,20 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.btnToSimplified = new System.Windows.Forms.Button();
+            this.btnToTraditional = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtShow
+            // 
+            this.txtShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtShow.Location = new System.Drawing.Point(12, 41);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtShow.Size = new System.Drawing.Size(554, 308);
+            this.txtShow.TabIndex = 0;
+            // 
+            // btnToSimplified
+            // 
+            this.btnToSimplified.Location = new System.Drawing.Point(12, 12);
+            this.btnToSimplified.Name = "btnToSimplified";
+            this.btnToSimplified.Size = new System.Drawing.Size(75, 23);
+            this.btnToSimplified.TabIndex = 1;
+            this.btnToSimplified.Text = "转为简体";
+            this.btnToSimplified.UseVisualStyleBackColor = true;
+            this.btnToSimplified.Click += new System.EventHandler(this.btnToSimplified_Click);
+            // 
+            // btnToTraditional
+            // 
+            this.btnToTraditional.Location = new System.Drawing.Point(93, 12);
+            this.btnToTraditional.Name = "btnToTraditional";
+            this.btnToTraditional.Size = new System.Drawing.Size(75, 23);
+            this.btnToTraditional.TabIndex = 1;
+            this.btnToTraditional.Text = "转为繁体";
+            this.btnToTraditional.UseVisualStyleBackColor = true;
+            this.btnToTraditional.Click += new System.EventHandler(this.btnToTraditional_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(491, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(410, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // FormMain
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnToTraditional);
+            this.Controls.Add(this.btnToSimplified);
+            this.Controls.Add(this.txtShow);
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "繁体简体字幕转换工具";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FormMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FormMain_DragEnter);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.Button btnToSimplified;
+        private System.Windows.Forms.Button btnToTraditional;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- ChineseSubtitleConversionTool/FormMain.cs
+++ ChineseSubtitleConversionTool/FormMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -15,5 +16,136 @@ namespace ChineseSubtitleConversionTool
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 初始化窗体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 获取拖拽信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// 解析拖拽信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            try
+            {
+                // 创建一个 StreamReader 的实例来读取文件 
+                // using 语句也能关闭 StreamReader
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    StringBuilder sbText = new StringBuilder();
+                    // 从文件读取并显示行，直到文件的末尾 
+                    while (!sr.EndOfStream)
+                    {
+                        sbText.AppendLine(sr.ReadLine());
+                    }
+                    txtShow.Text = sbText.ToString();
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        /// <summary>
+        /// 转为简体按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnToSimplified_Click(object sender, EventArgs e)
+        {
+            txtShow.Text = StringToSimlified(txtShow.Text);
+        }
+
+        /// <summary>
+        /// 转为繁体按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnToTraditional_Click(object sender, EventArgs e)
+        {
+            txtShow.Text = StringToTraditional(txtShow.Text);
+        }
+
+        /// <summary>
+        /// 字符串转为简体中文
+        /// </summary>
+        /// <param name="str">简体中文字符串</param>
+        /// <returns>繁体中文字符串</returns>
+        public string StringToSimlified(string str)
+        {
+            try
+            {
+                return Microsoft.VisualBasic.Strings.StrConv(str, Microsoft.VisualBasic.VbStrConv.SimplifiedChinese, 0);
+            }
+            catch (Exception)
+            {
+                return "";
+            } 
+        }
+
+        /// <summary>
+        /// 清空文本框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtShow.Clear();
+        }
+
+        /// <summary>
+        /// 保存文本框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 字符串转为繁体字
+        /// </summary>
+        /// <param name="str">简体中文字符串</param>
+        /// <returns>繁体中文字符串</returns>
+        public string StringToTraditional(string str)
+        {
+            try
+            {
+                return Microsoft.VisualBasic.Strings.StrConv(str, Microsoft.VisualBasic.VbStrConv.TraditionalChinese, 0);
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
     }
 }
