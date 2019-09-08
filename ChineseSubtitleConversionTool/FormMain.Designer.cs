03d1a212c2ad4ba8704b97b4b3b5ab40d1ac6531
Chishin <pro@xxgzs.org>
2019-09-08 09:29:22 +0800

    增加拖拽功能 增加批量转换UI

--- ChineseSubtitleConversionTool/FormMain.Designer.cs
+++ ChineseSubtitleConversionTool/FormMain.Designer.cs
@@ -33,6 +33,16 @@
             this.btnToTraditional = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnOpenPath = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnStartConvert = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbFormat = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtShow
@@ -44,7 +54,7 @@
             this.txtShow.Multiline = true;
             this.txtShow.Name = "txtShow";
             this.txtShow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtShow.Size = new System.Drawing.Size(554, 308);
+            this.txtShow.Size = new System.Drawing.Size(554, 229);
             this.txtShow.TabIndex = 0;
             // 
             // btnToSimplified
@@ -89,23 +99,126 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.cbFormat);
+            this.groupBox1.Controls.Add(this.btnStartConvert);
+            this.groupBox1.Controls.Add(this.btnOpenPath);
+            this.groupBox1.Controls.Add(this.txtFileName);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 276);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(554, 73);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "批量转换";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "目录：";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(55, 18);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(412, 21);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnOpenPath
+            // 
+            this.btnOpenPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenPath.Location = new System.Drawing.Point(473, 16);
+            this.btnOpenPath.Name = "btnOpenPath";
+            this.btnOpenPath.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenPath.TabIndex = 2;
+            this.btnOpenPath.Text = "浏览";
+            this.btnOpenPath.UseVisualStyleBackColor = true;
+            this.btnOpenPath.Click += new System.EventHandler(this.btnOpenPath_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "文件名：";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileName.Location = new System.Drawing.Point(198, 45);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(269, 21);
+            this.txtFileName.TabIndex = 1;
+            this.txtFileName.Text = "{name}.{exten}";
+            // 
+            // btnStartConvert
+            // 
+            this.btnStartConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartConvert.Location = new System.Drawing.Point(473, 43);
+            this.btnStartConvert.Name = "btnStartConvert";
+            this.btnStartConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnStartConvert.TabIndex = 2;
+            this.btnStartConvert.Text = "开始转换";
+            this.btnStartConvert.UseVisualStyleBackColor = true;
+            this.btnStartConvert.Click += new System.EventHandler(this.btnStartConvert_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "格式：";
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Items.AddRange(new object[] {
+            "转为简体",
+            "转为繁体"});
+            this.cbFormat.Location = new System.Drawing.Point(55, 45);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(81, 20);
+            this.cbFormat.TabIndex = 3;
+            // 
             // FormMain
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnToTraditional);
             this.Controls.Add(this.btnToSimplified);
             this.Controls.Add(this.txtShow);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "繁体简体字幕转换工具";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FormMain_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FormMain_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,6 +231,15 @@
         private System.Windows.Forms.Button btnToTraditional;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbFormat;
+        private System.Windows.Forms.Button btnStartConvert;
+        private System.Windows.Forms.Button btnOpenPath;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ChineseSubtitleConversionTool/FormMain.cs
+++ ChineseSubtitleConversionTool/FormMain.cs
@@ -12,6 +12,10 @@ namespace ChineseSubtitleConversionTool
 {
     public partial class FormMain : Form
     {
+        private ToolTip TipObject;
+        private string OpenFileDefineExt = "";
+        private string OpenFileDefineName = "";
+
         public FormMain()
         {
             InitializeComponent();
@@ -24,7 +28,17 @@ namespace ChineseSubtitleConversionTool
         /// <param name="e"></param>
         private void FormMain_Load(object sender, EventArgs e)
         {
+            cbFormat.SelectedIndex = 0;
 
+            TipObject = new ToolTip();
+            TipObject.AutoPopDelay = 5000;    //工具提示保持可见的时间期限
+            TipObject.InitialDelay = 200;     //鼠标放上，自动打开提示的时间
+            TipObject.ReshowDelay = 1000;       //鼠标离开，自动关闭提示的时间
+            TipObject.ShowAlways = true;     //总是显示，即便空间非活动
+            TipObject.UseAnimation = true;   //动画效果
+            TipObject.UseFading = true;      //淡入淡出效果
+            TipObject.IsBalloon = true;      //气球状外观
+            TipObject.SetToolTip(this.txtFileName, "替换符说明：{name}原文件名称，{exten}文件扩展名，{num}文件序号");
         }
 
         /// <summary>
@@ -52,24 +66,33 @@ namespace ChineseSubtitleConversionTool
         private void FormMain_DragDrop(object sender, DragEventArgs e)
         {
             string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
-            try
+            if (File.Exists(path))
             {
-                // 创建一个 StreamReader 的实例来读取文件 
-                // using 语句也能关闭 StreamReader
-                using (StreamReader sr = new StreamReader(path))
+                try
                 {
-                    StringBuilder sbText = new StringBuilder();
-                    // 从文件读取并显示行，直到文件的末尾 
-                    while (!sr.EndOfStream)
+                    OpenFileDefineExt = Path.GetExtension(path);
+                    OpenFileDefineName = Path.GetFileNameWithoutExtension(path);
+                    // 创建一个 StreamReader 的实例来读取文件 
+                    // using 语句也能关闭 StreamReader
+                    using (StreamReader sr = new StreamReader(path))
                     {
-                        sbText.AppendLine(sr.ReadLine());
+                        StringBuilder sbText = new StringBuilder();
+                        // 从文件读取并显示行，直到文件的末尾 
+                        while (!sr.EndOfStream)
+                        {
+                            sbText.AppendLine(sr.ReadLine());
+                        }
+                        txtShow.Text = sbText.ToString();
                     }
-                    txtShow.Text = sbText.ToString();
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
                 }
             }
-            catch (Exception err)
+            else if (Directory.Exists(path))
             {
-                Console.WriteLine(err.Message);
+                txtPath.Text = path.Trim();
             }
         }
 
@@ -127,7 +150,95 @@ namespace ChineseSubtitleConversionTool
         /// <param name="e"></param>
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if (txtShow.Text.Trim() != "")
+            {
+                if(SaveFile(txtShow.Text))
+                {
+                    MessageBox.Show("您的字幕文件已保存完毕。", "保存完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    Console.WriteLine("触发保存但没有保存成功。");
+                }
+            }
+            else
+            {
+                MessageBox.Show("缓存数据为空。", "无法保存", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
 
+        /// <summary>
+        /// 浏览按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenPath_Click(object sender, EventArgs e)
+        {
+            string path = OpenFolder();
+            if (Directory.Exists(path))
+            {
+                txtPath.Text = path;
+            }
+        }
+
+        /// <summary>
+        /// 开始转换按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStartConvert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 打开文件夹
+        /// </summary>
+        /// <returns></returns>
+        public string OpenFolder()
+        {
+            string sPath = "";
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择读取目录";  //定义在对话框上显示的文本
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                sPath = folder.SelectedPath;
+            }
+            return sPath;
+        }
+
+        /// <summary>
+        /// 保存文件
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public bool SaveFile(string text)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "保存字幕文件";
+            sfd.DefaultExt = OpenFileDefineExt;
+            sfd.FileName = OpenFileDefineName;
+            sfd.Filter = "ASS文件 (*.ass)|*.ass|SSA文件 (*.ssa)|*.ssa|SRT文件 (*.srt)|*.srt|LRC文件 (*.lrc)|*.lrc|文本文件 (*.txt)|*.txt|所有文件 (*.*)|*.*";
+            if (sfd.ShowDialog() != DialogResult.OK)
+            {
+                return false;
+            }
+            string path = sfd.FileName;
+            try
+            {
+                using (FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(text);
+                    fsWrite.Write(buffer, 0, buffer.Length);
+                    return true;
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                MessageBox.Show(err.Message, "保存失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
         }
 
         /// <summary>
