e8d7559fd75cc1d76ca6cbeb5c57a2a99bbf23f6
chishin <pro@xxgzs.org>
2024-02-27 13:35:49 +0800

    重新创建项目V1.x

--- ChineseSubtitleConversionTool/FormMain.Designer.cs
+++ ChineseSubtitleConversionTool/FormMain.Designer.cs
@@ -34,8 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(464, 361);
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "繁体简体字幕转换工具";
             this.ResumeLayout(false);
 
--- ChineseSubtitleConversionTool/Properties/AssemblyInfo.cs
+++ ChineseSubtitleConversionTool/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chishin")]
 [assembly: AssemblyProduct("Chinese Subtitle Conversion Tool")]
-[assembly: AssemblyCopyright("Copyright © Chishin 2019")]
+[assembly: AssemblyCopyright("Copyright © Chishin 2019-2024")]
 [assembly: AssemblyTrademark("www.xxgzs.org")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
